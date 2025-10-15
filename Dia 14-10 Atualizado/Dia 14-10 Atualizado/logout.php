cdc0fbdffffa33f72f4ba8f25dcca21d9434c196
Dev_Layanne <layannesousalay@gmail.com>
2025-10-15 16:56:54 -0300

    Delete Dia 14-10 Atualizado/Dia 14-10 Atualizado directory

--- Dia 14-10 Atualizado/Dia 14-10 Atualizado/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start(); // Inicia a sessão
-
-session_unset(); // Remove todas as variáveis da sessão
-
-session_destroy(); // Destrói a sessão
-
-header("Location: telaPrincipal.php"); // Redireciona para a página principal (agora .php)
-exit;
