494c02efc1c64408869fc31ef0147e44f9082aa8
Dev_Layanne <layannesousalay@gmail.com>
2025-12-14 18:56:25 -0300

    Delete ChampionsSports/P_Final/P_Final 01-12/P_Final directory

--- ChampionsSports/P_Final/P_Final 01-12/P_Final/php/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start(); // Inicia a sessão
-
-session_unset(); // Remove todas as variáveis da sessão
-
-session_destroy(); // Destrói a sessão
-
-header("Location: ../index.php"); // Redireciona para a página principal na pasta raiz
-exit;
